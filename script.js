253d554ae069f1c46442c95cc90caf356299cea3
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-11 22:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const p=document.getElementById("counter");
+const button = document.getElementById("incrementBtn");
+
+button.addEventListener("click" , Onclick);
+
+function Onclick(event) {
+	event.preventDefault();
+	let increment=parseInt(p.innerText);
+	
+	p.innerText=++increment;
+	
+}
